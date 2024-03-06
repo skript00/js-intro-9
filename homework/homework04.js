9c43aa1d56510139f85a923366be15a2a1cd1309
ameers00 <am33rsaleh6@gmail.com>
2024-03-05 18:08:04 -0600

    .

--- homework/homework04.js
+++ homework/homework04.js
@@ -197,7 +197,9 @@ function getDuplicates(arr) {
     let duplicate = [];
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] === arr[j] )  
+            if (arr[i] === arr[j] )  {
+                
+            }
         }
     }
 }
