c4b0047d357b62dbd52b7755ea8e10f84f4524b8
ameers00 <am33rsaleh6@gmail.com>
2024-03-23 13:40:10 -0500

    .

---
+++ js-async/01-stack.js
@@ -0,0 +1,27 @@
+// Synchronous code
+console.log('1');
+console.log('2');
+console.log('3');
+
+
+
+//
+function method1() {
+    console.log('1');
+}
+
+function method2() {
+    console.log('2');
+}
+
+function method3() {
+    console.log('3');
+}
+
+function program() {
+    method1();
+    method2();
+    method3();
+}
+
+program();
